2c362f89b889f3b741380026dcd7750846271258
Bryana <bryananwall@gmail.com>
2019-03-31 16:27:01 -0500

    Add classes to JS file

--- src/main.js
+++ src/main.js
@@ -1,2 +1,23 @@
 //add a class called desserts and pass two properties called type and calories
 
+class Desserts{
+    constructor (type, calories){
+        this.type = type;
+        this.calories = calories;
+    }
+    
+}
+
+class Icecream extends Desserts{
+    constructor (flavor, scoops){
+        this.flavor = flavor;
+        this.calories = calories;
+    }
+}
+
+includeSpoon(){
+    console.log("Here is your spoon!");
+}
+
+const Icecream = new Icecream ("icecream", 340, "vanilla");
+console.log (Icecream);
